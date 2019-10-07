df6fa28943f2dbfbffbb602e9b4a3c0d558e9b6c
Mokshi Vyas <vyasmokshi31@gmail.com>
2019-10-07 16:02:02 -0700

    added constructor injection simple property

---
+++ ConstructorInjection/src/main/java/com/zensar/spring/main/ZensarIdolMain.java
@@ -0,0 +1,17 @@
+package com.zensar.spring.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.zensar.spring.performers.Juggler;
+import com.zensar.spring.performers.Performer;
+
+public class ZensarIdolMain {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("performers.xml");
+		Performer p = ctx.getBean("Pyare Mohan",Performer.class);
+		p.perform();
+	}
+
+}
